b23060f72b863ecd52d9763c4a126f551f925441
Petr Müller <muller.contact@gmail.com>
2018-02-03 20:18:35 +0100

    Graph - slow progress

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
-var data = {
-  // A labels array that can contain any sort of values
-  labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-  // Our series array that contains series objects or in this case series data arrays
-  series: [
-    [5, 2, 4, 2, 0]
-  ]
-};
+// Initialize a Line chart in the container with the ID chart1
+  new Chartist.Bar('#chart1', {
+    labels: ["8:00","","8:00","","8:00","","8:00","","8:00","","8:00","","8:00","","8:00","","8:00","","8:00","","8:00","","8:00","","8:00","","8:00","","22:00",""],
+    series: [[100, 10, 120, 180, 200, 100, 10, 120, 180, 200, 100, 10, 120, 180, 200, 100, 10, 120, 180, 200,100, 10, 120, 180, 200, 100, 10, 120, 180]]
+	}, 
 
-// Create a new line chart object where as first parameter we pass in a selector
-// that is resolving to our chart container element. The Second parameter
-// is the actual data object.
-new Chartist.Line('.ct-chart', data);
+	{
+
+	axisX: {
+   		showGrid: false,
+	},
+
+	axisY: {
+   		showLabel: false,
+   		showGrid: false,
+	},
+
+	seriesBarDistance: 20,
+  	
+
+  });
+
+  // Initialize a Line chart in the container with the ID chart2
+  new Chartist.Bar('#chart2', {
+    labels: [1, 2, 3, 4],
+    series: [[5, 2, 8, 3]]
+  });
+
+
